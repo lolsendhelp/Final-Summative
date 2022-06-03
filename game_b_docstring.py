fc19704475c8b5ed8a655126efe5e73f275ed246
lolsendhelp <47632981+lolsendhelp@users.noreply.github.com>
2022-06-03 13:02:54 -0400

    Create game_b_docstring.py

---
+++ game_b_docstring.py
@@ -0,0 +1,35 @@
+def battle_ship():
+    
+    def draw_grid():
+        """""
+        turtle will create 5x5 grid 
+        """
+    
+    def draw_battleship():
+        "Turtle will draw battleship"
+        
+    def draw_cruiser():
+        "Turtle will draw cruiser"
+    
+    def draw_submarine():
+        "Turtle will draw submarine"
+
+    
+    print("Rules of the game \n\
+1. You must destroy every single boat in order to win the game \n\
+2. You must choose a location on the grid in order to call a hit \n\
+3. You must input your choices in the format of column and then row. Example (A5)")
+    
+    ship_row = random_row(board)
+    ship_col = random_col(board)
+    
+    ship_row = random_row(board)
+    ship_col = random_col(board)
+    
+    ship_row = random_row(board)
+    ship_col = random_col(board)
+    
+    ship_row = random_row(board)
+    ship_col = random_col(board)
+    
+    while 
