134fce2eb8afbf6be9e19c6b4766207fe8405ca7
lolsendhelp <47632981+lolsendhelp@users.noreply.github.com>
2022-06-08 14:44:07 -0400

    Update game_b_docstring.py

--- game_b_docstring.py
+++ game_b_docstring.py
@@ -17,28 +17,82 @@ def battleship():
     enemy_health = 9
     turns = 0
     
+    enemy_place_battleship = random.choice(grid_locations_opponent)
+    for i in range(4):
+        enemy_battleship_location = enemy_place_battleship
+        enemy_place_battleship = enemy_place_battleship + 1
+        grid_locations_opponent.remove(enemy_battleship_location)
+        
+    enemy_place_cruiser = random.choice(grid_locations_opponent)
+    for i in range(4):
+        enemy_cruiser_location = enemy_place_cruiser
+        enemy_place_cruiser = enemy_place_cruiser + 1
+        grid_locations_opponent.remove(enemy_cruiser_location)
+        
+    enemy_place_submarine = random.choice(grid_locations_opponent)
+    for i in range(4):
+        enemy_submarine_location = enemy_place_submarine
+        enemy_place_submarine = enemy_place_submarine + 1
+        grid_locations_opponent.remove(enemy_submarine_location)
+    
     def draw_grid():
         """""
         turtle will create 5x5 grid 
         """
+        pass
     
     def draw_battleship():
         "Turtle will draw 4x1 battleship"
+        pass
         
     def draw_cruiser():
         "Turtle will draw 3x1 cruiser"
+        pass
     
     def draw_submarine():
         "Turtle will draw 2x1 submarine"
-    
+        pass 
 
+    def player_choice():
+        player_choice = int(input("Where do you want to hit?: "))
+        hit = 0
+        
+        if player_choice in enemy_battleship_location:
+            print("HIT! \n\
+You hit their battleship!")
+            enemy_battleship_location.remove(player_choice)
+            hit = hit + 1
+            
+        if player_choice in enemy_cruiser_location:
+            print("HIT! \n\
+You hit their cruiser!")
+            enemy_cruiser_location.remove(player_choice)
+            hit = hit + 1
+        
+        if player_choice in enemy_submarine_location:
+            print("HIT! \n\
+You hit their submarine!")
+            enemy_submarine_location.remove(player_choice)
+            hit = hit + 1
+            
+        if player_choice not in enemy_battleship_location or enemy_cruiser_location or enemy_submarine_location and player_choice in grid_locations_opponent:
+            print("MISS! \N\
+You hit nothing!")
+        
+        if player_choice not in enemy_battleship_location or enemy_cruiser_location or enemy_submarine_location and player_choice not in grid_locations_opponent:
+            print()
+        
+        return hit
     
     print("Rules of the game \n\
 1. You must destroy every single boat in order to win the game \n\
 2. You must choose a location on the grid in order to call a hit \n\
 3. Each space of the grid has a number associated with it \n\
+4. If a player gets a hit they are given a chance to try again \n\
+5. If a player misses they are not given a chance to go again \n\
 the numbers start at the bottom left of the board and go up until 25 \n\
-which is the top right of the board.")
+which is the top right of the board. \n\
+")
     
     battleship_orientation = input("What orientation would you like the battleship to be? (vert/hor): ")
     battleship_orientation = battleship_orientation.lower()
@@ -49,15 +103,16 @@ your battleship on: "))
         for i in range(4):
             player_battleship_location = place_battleship
             place_battleship = place_battleship + 3
-        grid_locations_player.remove(player_battleship_location)   
+            grid_locations_player.remove(player_battleship_location) 
+            
     
     if battleship_orientation == "hor":
         for i in range(4):
             player_battleship_location = place_battleship
             place_battleship = place_battleship + 1
-        grid_locations_player.remove(player_battleship_location)
+            grid_locations_player.remove(player_battleship_location)
     
-    cruiser_orientation = input("What orientation would you like the submarine to be? (vert/hor): ")
+    cruiser_orientation = input("What orientation would you like the cruiser to be? (vert/hor): ")
     cruiser_orientation = cruiser_orientation.lower()  
     place_cruiser = int(input("Please enter the location you want to position \n\
 your cruiser on: "))
@@ -66,13 +121,13 @@ your cruiser on: "))
         for i in range(4):
             player_cruiser_location = place_cruiser
             place_cruiser = place_cruiser + 3
-        grid_locations_player.remove(player_cruiser_location)
+            grid_locations_player.remove(player_cruiser_location)
         
     if cruiser_orientation == "hor":
         for i in range(4):
             player_cruiser_location = place_cruiser
             place_cruiser = place_cruiser + 1
-        grid_locations_player.remove(player_cruiser_location)
+            grid_locations_player.remove(player_cruiser_location)
         
     submarine_orientation = input("WHat orientation would you like the submarine to be? (vert/hor): ")
     submarine_orientation = submarine_orientation.lower()
@@ -83,21 +138,13 @@ your submarine on: "))
         for i in range(4):
             player_submarine_location = place_submarine
             place_submarine = place_submarine + 3
-        grid_locations_player.remove(player_submarine_location)    
+            grid_locations_player.remove(player_submarine_location)    
         
     if submarine_orientation == "hor":
         for i in range(4):
             player_submarine_location = place_submarine
             place_submarine = place_submarine + 1
-        grid_locations_player.remove(player_submarine_location)
-    
-    #places the 
-    enemy_place_battleship = random.choice(grid_locations_opponent)
-    for i in range(4):
-        enemy_battleship_location = enemy_place_battleship
-        enemy_place_battleship = enemy_place_battleship + 1
-        
-    
+            grid_locations_player.remove(player_submarine_location)
     
     #Start of main game 
     draw_grid()
@@ -108,4 +155,14 @@ your submarine on: "))
     #draw_submarine()
     
     while player_health > 0 and enemy_health > 9 and turns > 25:
-        player_choice = int(input("Where do you want to hit?: "))
+        hit = player_choice()
+        
+        if hit == 1:
+            player_choice()
+        
+        elif hit 
+        
+
+            
+        
+battleship()
